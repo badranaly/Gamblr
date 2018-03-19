9771ee6bad09f3c2c90c5800e8c01e18d77cf34a
Lillian Shieh <lillian.shieh95@gmail.com>
2018-03-19 12:32:15 -0400

    Feed

---
+++ client/src/Feed.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Feed extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+
+		)
+	}
+}
