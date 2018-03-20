18e1fffeb967dd54c22f2835ca6bde05f1cbb956
Lillian Shieh <lillian.shieh95@gmail.com>
2018-03-20 11:05:43 -0400

    got server running

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,25 @@ import {BrowserRouter as Router,Route,Link} from 'react-router-dom'
 import './App.css';
 
 import Login from './Login'
-import SignUp from './signup'
+import SignUp from './Signup'
 import Feed from './Feed'
+import Likes from './Likes'
 import Userpage from './Userpage'
+//import Following from './Following'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <Route exact path='/' component={Login} />
           <Route path='/login' component={Login} />
           <Route path='/signup' component={SignUp} />
           <Route path='/feed' component={Feed} />
-          <Route path='/post/:id' component={} />
+          {/*<Route path='/post/:id' component={} />*/}
           <Route path='/:username' component={Userpage} />
           <Route path='/favs' component={Likes} />
-          <Route path='/following', component={Following} />
-          <Route path='/myposts', component={MyPosts} />
+          {/*<Route path='/following' component={Following} />*/}
         </div>
       </Router>
     )
