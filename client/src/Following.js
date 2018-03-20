d5999ebd577a25796b7659c9572f7ea7d8b92b90
Aly Badran <aly2cool@hotmail.com>
2018-03-20 11:23:56 -0400

    merging with lilComponent

---
+++ client/src/Following.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import services from './services/apiServices'
+
+class Following extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return (
+
+    )
+  }
+}
+
+export default Following
