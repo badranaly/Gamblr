7ac463095e5c2569818ac745695802513b72d8e0
Aly Badran <aly2cool@hotmail.com>
2018-03-20 16:20:39 -0400

    pushing login and signup to dev for testing

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,12 +1,38 @@
 //LetsGetit
 
 import React, {Component} from 'react'
+import Redirect from 'react-router-dom'
+import services from './services/apiServices'
 
 class Login extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      user_name: username,
+      password: password,
+      fireRedirect: false
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(e){
+      e.preventDefault()
+      services.authenticateUser(this.state.user_name, this.state.password)({
+        console.log('initiating login procedure..')
+        .then(data => {
+          if(data.username == this.state.user_name){
+            this.setState({
+              fireRedirect: true
+            })
+          }
+        })
+        .catch()
+      })
+  }
   render(){
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input name='username' placeholder='Username' type='text' /><br />
           <input name='password' type='password' />
           <input type='submit' />
