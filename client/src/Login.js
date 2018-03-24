e32dc5b8561ac30eddc594b16d1fd1e623f6b9e0
Chris Harding <christopher.harding@gmx.com>
2018-03-24 16:23:48 -0400

    logic to handle good or bad logins add

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,8 @@
 //LetsGetit
 
 import React, {Component} from 'react'
+import {Redirect} from 'react-router-dom'
+
 import services from './services/apiServices'
 import './Login.css'
 
@@ -11,7 +13,8 @@ class Login extends Component {
       user_name: '',
       password: '',
       authenticated: false,
-      fireRedirect: false
+      fireRedirect: false,
+      failedLogin: null
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -23,6 +26,15 @@ handleChange(e){
   this.setState({
     [name]:value
   })
+}
+
+renderError() {
+  return(
+  <div className="alert">
+    <span className="closebtn"></span>
+    Invalid Credentials -- please try again.
+  </div>
+  )
 }
 
 handleSubmit(e){
@@ -37,9 +49,15 @@ handleSubmit(e){
       .catch(err => console.log('loggin is fucked up', err)) */
       services.userLogin(this.state.user_name, this.state.password)
       .then(user => {
+        this.setState({
+          fireRedirect: true
+        })
         console.log("SUCCESS")
       })
       .catch(err => {
+        this.setState({
+          failedLogin: true
+        })
         console.log("ERROR")
       })
   }
@@ -55,8 +73,10 @@ handleSubmit(e){
               <input type="password" onChange={this.handleChange} class="form-control" name="password" placeholder="Password" required=""/>
               <button class="btn btn-lg btn-danger btn-block" type="submit">Login</button>
             </form>
+            {this.state.failedLogin ? this.renderError(): ''}
           </div>
         </form>
+        {this.state.fireRedirect ? <Redirect to={'/feed'} /> : ''}
       </div>
     )
   }
